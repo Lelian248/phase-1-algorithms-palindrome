5443186d8a450ad1ad7a213525b2064dd2389b2f
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-23 04:46:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordLength = word.length
+  for(let i=0 ; i < wordLength/2 ;i++){
+    if (word[i] !== word[wordLength -1 - i]){
+      console.log("fales")
+      return false
+    } 
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  1- fined the word length 
+  2- loop throught the word and check if the element index equal to the elemnt length minus th index 
+  if no return fales then  divide the length t half so we can remove the first and laast element and compare the rest 
+  we keep looping until the index become bigger than the length\2 if yes return true 
 */
 
 /*
